cc9feefb242afcac691f19cc2bc9cb6ee6c9a269
SantiagoDeStefano <khoinguyenpkn@gmail.com>
2024-02-29 11:17:57 +0700

    tinhS_nuaHinhTron

---
+++ tinhSnuahtron.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double long db;
+typedef string str;
+const ll NMAX = 1e6;
+const ll lim = 1e18;
+ll a[NMAX + 10];
+
+void solve() {
+    db a;
+    cin >> a;
+    cout << fixed << setprecision(3) << a * a * M_PI / 2;
+}
+
+int main()
+{
+ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0) ;
+ #ifndef ONLINE_JUDGE
+   freopen("main.inp", "r", stdin);
+   freopen("main.out", "w", stdout);
+#endif
+    solve();
+}
